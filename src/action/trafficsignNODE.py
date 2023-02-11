8f23b9db46f206d3e033d3a4890ae2274926472d
root <khanhtrinh81103@gmail.com>
2023-02-11 18:16:42 +0000

    src/action/trafficsignNODE.py

--- src/action/trafficsignNODE.py
+++ src/action/trafficsignNODE.py
@@ -12,29 +12,54 @@ from control.controlNODE import controlNODE
 class trafficsignNODE():
     def traffic_sign_processing(self, msg):
         traffic_sign = msg.traffic_sign_type
+
         if traffic_sign == 6: #detech a STOP sign
-            print("Stop at least 3 seconds")
-            controlNODE().brake(0)
+            print("Stop at least 3 seconds") 
+            self.control.brake(0) # ???
             start_time = time.time()
             while((time.time() - start_time) > 3):
                 pass   
+
         elif traffic_sign == 3: #packing place
             print("pass")
+            #call parking funtion
+
         elif traffic_sign == 0: #crosswalk
             print("Slow down")
-            controlNODE().brake(0)
-            sleep(1)
-        elif traffic_sign == 4: #priority walk
+            #controlNODE().brake(0)
+            #sleep(1)
+            self.control.setSpeed(0.1)
+            #after pass the crosswalk -> then what(go straight)
+            self.control.setSpeed(0.3, 0.3)
+
+        elif traffic_sign == 4: #priority road
             print("Do not stop at intesection")
+            #intersction -> cross walk -> 1: move forward, 2: turn right/left
+            self.control.moveForward(0.3, 0.3)
+
+
         elif traffic_sign ==  2: #one way road
             print("go straight")
-            controlNODE().setSpeed(0.3)
-        elif traffic_sign == 1:
+            self.control.setSpeed(0.3)
+
+        elif traffic_sign == 1: #no entry road
             print("turn other way")
-            controlNODE().setSteer(23)
-            controlNODE().moveForward(0.3, 0.3)
-            controlNODE().setSteer(0)
-            controlNODE().setSpeed(0.3)
+            self.control.setSteer(23)
+            self.control.moveForward(0.3, 0.3)
+            self.control.setSteer(0)
+            self.control.setSpeed(0.3)
+
+        elif traffic_sign == 7 #roundabout
+            self.control.setSteer(23)
+            self.control.moveForward(0.2,0.1)
+            self.control.setSteer(-23)
+            self.control.moveForward(0.2,0.1)
+            self.control.setSteer(10)
+            self.control.moveForward(0.2,0.1)
+            self.control.setSteer(0)
+            self.control.setSpeed(0.3)
+
+        
 
     def __init__(self): 
         rospy.init_node('trafficsignNODE', anonymous = False)
