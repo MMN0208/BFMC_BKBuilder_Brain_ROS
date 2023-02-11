aaa6e6e9bc088f2cd11f7201d546a08cb8b19f4a
Tien <tien.le.290803@gmail.com>
2023-02-11 06:44:45 +0000

    parkingNODE

---
+++ src/action/parkingNODE.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import cv2
+import sys
+sys.path.append('../') 
+import rospy
+from enum import Enum
+import time
+from utils.msg import Parking
+from control import controlNODE
+
+class parkingNODE():
+    def traffic_sign_parking_processing(self, msg):
+        traffic_sign_type = msg.traffic_sign_type
+        slot1 = msg.slot1
+        slot2 = msg.slot2
+        parking_type = msg.parking_type
+        if traffic_sign_type == 3 and (slot1 == 1 or slot2 == 1):
+            if parking_type == 0:
+                if slot1 == 1 and slot2 == 0:
+                    print("start parking parallel at slot 1")
+                    self.parking_parallel(slot1, slot2)
+                elif slot1 == 0 and slot2 == 1:
+                    print("start parking parallet at slot 2")
+                    self.parking_parallel(slot1, slot2)
+            elif parking_type == 1:
+                if slot1 == 1 and slot2 == 0:
+                    print("start parking perpendicular at slot 1")
+                    self.parking_perpendicular(slot1,slot2)
+                elif slot1 == 0 and slot2 == 1:
+                    print("start parking perpendicular at slot 2")
+                    self.parking_perpendicular(slot1,slot2)
+        else:
+            print("pass")
+
+    def __init__(self):
+        rospy.init_node('parkingNODE', anonymous=False)
+        self.parking_subscriber = rospy.Subscriber("/automobile/parking", Parking, self.traffic_sign_parking_processing)
+        self.unlock = 1
+        self.lock = 0
+
+    def run(self):
+        rospy.loginfo('starting parkingNODE')
+        self._read()
+    
+    def _read(self):
+        while not rospy.is_shutdown():
+            try:
+                print("Hello from parking")
+                time.sleep(2)
+            except UnicodeDecodeError:
+                pass
+
+    def lock_state():
+        if self.unlock:
+            self.lock = 1
+            self.unlock = 0
+            self.time_lock_start = rospy.get_time()
+
+    def unlock_state(self, state, time):
+        if self.lock:
+            if (rospy.get_time() - self.time_lock_start) < time:
+                self.run_state = state
+            else: 
+                self.unlock = 1
+                self.lock = 0
+    def parking_perpendicular(self, slot1, slot2):
+        if slot1 == 1 and slot2 == 0:
+            controlNODE.setSteer(23)
+            controlNODE.moveForward(0.5, 0.3)
+        elif slot1 == 0 and slot2 == 1:
+            controlNODE.setSteer(23)
+            controlNODE.moveForward(-0.5, 0.3)
+
+    def parking_parallel(self, slot1, slot2):
+        if slot1 == 1 and slot2 == 0:
+            controlNODE.setSteer(23)
+            controlNODE.moveForward(0.5, 0.3)
+        elif slot1 == 0 and slot2 == 1:
+            controlNODE.setSteer(23)
+            controlNODE.moveForward(-0.5, 0.3)
+
+if __name__ == "__main__":
+    parkNODE = parkingNODE()
+    parkNODE.run()
