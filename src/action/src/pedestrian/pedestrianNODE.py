d37ceb80f9ed0178c4a97fc21516beca4905c6ae
Hai Pham <76871780+haiphamcse@users.noreply.github.com>
2023-02-12 13:24:37 +0700

    Add files via upload

--- src/action/src/pedestrian/pedestrianNODE.py
+++ src/action/src/pedestrian/pedestrianNODE.py
@@ -6,25 +6,41 @@ import rospy
 from std_msgs.msg      import String
 from utils.srv         import subscribing, subscribingResponse
 from utils.msg         import pedestrian, pedestrian_output
-from line_boundary_check import *
-from cv_bridge       import CvBridge
+#from yolov8detect      import *
+from cv_bridge         import CvBridge
 import cv2
+
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+
+"""
+Logs: 12/2/23 (hai): Added code for pedestrian detection
+     - When a pedestrian is detected within a predefined area (self.points), the code will publish a pedestrian_output msg to pedestrian_test with
+     values of 1 (otherwise values will be 0)
+     - To run the code:
+        + pip install -r requirements.txt
+        + add following to your launch file
+        <node pkg="perception" type="objectNODE.py" 	name="objectNODE"  		    output="screen" cwd="node"/>
+        <node pkg="action" type="pedestrianNODE.py" 	name="pedestrianNODE"  		output="screen" cwd="node"/>
+     - Notes:
+        + Adaptive area with respect to lane is not implemented (area is always fixed)
+        + Pedestrian class is not defined (currently the model treats pedestrian as a cross-walk sign)
+"""
+
 class pedestrianNODE():
     def __init__(self) -> None:
         self.pedestrian_subscriber = rospy.Subscriber("/automobile/pedestrian", pedestrian, self.check_pedestrian)
         self.lane_subscriber = rospy.Subscriber("/automobile/lane_info", String, self.check_lane)
         self.pedestrian_publisher = rospy.Publisher("/automobile/pedestrian_test", pedestrian_output, queue_size=1)
-        #
-        self.boundaryLines = [
-            boundaryLine([ 300,  40,  20, 400 ]),
-            boundaryLine([ 440,  40, 700, 400 ])
-        ]  
-        
-        self.areas = [
-            area([ [200,200], [500,180], [600,400], [300,300], [100,360] ])
-        ]
-        #
-        self.tracker = objectTracker()
+        self.bridge = CvBridge()
+        #PEDESTRIAN DETECTION
+        ih, iw = 1232, 1640
+        self.points = [[(iw-1500)//2, (ih-500)//2], #LEFT TOP
+          [(iw+1500)//2,(ih-500)//2], 
+          [(iw+900)//2, (ih+700)//2], 
+          [(iw-900)//2, (ih+700)//2],
+          [(iw-1500)//2, (ih-500)//2]]
         
     def run(self):
         """Apply the initializing methods and start the threads
@@ -33,35 +49,34 @@ class pedestrianNODE():
         #self._read() 
         rospy.spin()     
         
+    def isInside(self, points, centroid):
+        polygon = Polygon(points)
+        centroid = Point(centroid)
+        #print(polygon.contains(centroid))
+        return polygon.contains(centroid)   
+     
+    def draw_prediction(self, img, x, y, x_plus_w, y_plus_h, points):
+        color = (0, 255, 0)
+        print(f"TYPE:{type(x), type(y)}")
+        #cv2.rectangle(img, (int(x), int(y)), (int(x_plus_w), int(y_plus_h)), color, 2)
+        #cv2.putText(img, "", (int(x - 10), int(y - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+        # Tinh toan centroid
+        centroid = ((int(x) + int(x_plus_w)) // 2, (int(y) + int(y_plus_h)) // 2)
+        #cv2.circle(img, centroid, 5, (color), -1)
+
+        return self.isInside(points, centroid)
+        
     def check_pedestrian(self, msg):
         """Determined if a pedestrian is currently walking or not
         """
         image = self.bridge.imgmsg_to_cv2(msg.image, "bgr8")
-        print(image)
-        #if msg.is_pedestrian_infront:
-        px, py, ph, pw = msg.x, msg.y, msg.h, msg.w
-        #TODOS: Process and return is_pedestrian (bool)
-        #       Add pedestrian tracking
+        print(f"IMAGE FROM PEDESTRIAN:{image}")
         output = pedestrian_output()
         output.pedestrian = 0
-        
-        objects = None
-        self.tracker.trackObjects(objects)
-        self.tracker.evictTimeoutObjectFromDB()
-        self.tracker.drawTrajectory(outimg, objects)
-
-        checkLineCrosses(self.boundaryLines, objects)
-        drawBoundaryLines(image, boundaryLines)
-
-        checkAreaIntrusion(self.areas, objects)
-        for area in self.areas:
-            if area.count > 0:
-                output.pedestrian = 1
-        drawAreas(image, areas)
-        cv2.imshow("test", image)
-        cv2.waitKey(1)
-
-        #self.pedestrian_publisher.publish(output)
+        if(self.draw_prediction(image, msg.x, msg.y, msg.h, msg.w, self.points)):
+            output.pedestrian = 1
+        self.pedestrian_publisher.publish(output)
             
     def check_lane(self, msg):
         """Update lane values accordingly
@@ -76,10 +91,11 @@ class pedestrianNODE():
         float32: width_bottomright
         float32: heigth_bottomright
         """
-        self.boundaryLines = [
-            boundaryLine([msg.width_topleft, msg.height_topleft, msg.width_bottomleft, msg.height_bottomleft]),
-            boundaryLine([msg.width_topright, msg.heigth_topright, msg.width_bottomright, msg.heigth_bottomright])
-        ]
+        #TODOS: Update values with lane type
+        # self.boundaryLines = [
+        #     boundaryLine([msg.width_topleft, msg.height_topleft, msg.width_bottomleft, msg.height_bottomleft]),
+        #     boundaryLine([msg.width_topright, msg.heigth_topright, msg.width_bottomright, msg.heigth_bottomright])
+        # ]
 
 if __name__ == "__main__":
     print("Hello")
